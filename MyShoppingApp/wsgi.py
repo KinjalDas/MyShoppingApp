5cad200295d9491d34d219f392401f789c247687
suman sarkar <suman.sarkar@wipro.com>
2019-06-09 22:42:06 +0530

    heroku commit

--- MyShoppingApp/wsgi.py
+++ MyShoppingApp/wsgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+#from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyShoppingApp.settings')
-
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
+#application = DjangoWhiteNoise(get_wsgi_application())
