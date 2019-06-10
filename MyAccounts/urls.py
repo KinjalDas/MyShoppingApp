b8463d8d2218ca9ab5ff927ae04694467722ec27
Kinjal Das <kinjal.das86@gmail.com>
2019-06-10 15:12:59 +0530

    url correction

--- MyAccounts/urls.py
+++ MyAccounts/urls.py
@@ -6,7 +6,7 @@ app_name = 'MyAccounts'
 
 urlpatterns = [
     path('',views.index,name='authentication_check'),
-    path(r'register/$',views.register_user,name='register_user'),
-    path(r'login/$',views.login_user,name='login_user'),
-    path(r'logout/$',views.logout_user,name='logout_user'),
+    path(r'register/',views.register_user,name='register_user'),
+    path(r'login/',views.login_user,name='login_user'),
+    path(r'logout/',views.logout_user,name='logout_user'),
 ]
