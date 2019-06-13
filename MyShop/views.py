f4f66c7e4e371dd34765a634c01f487ee57882e1
suman sarkar <suman.sarkar@wipro.com>
2019-06-13 19:35:17 +0530

    adding MyShop corrections

--- MyShop/views.py
+++ MyShop/views.py
@@ -5,7 +5,16 @@ from MyShop.models import Category,Product
 
 def home(request):
     prod_list = Product.objects.order_by('-name')
+    print(prod_list)
     return render(request,'MyShop/index.html',{'products':prod_list})
 
 def details(request,pid):
-    return render(request,'MyShop/details.html',{'pid':pid,})
+    print(pid)
+    prod_list = Product.objects.all()
+    found = False
+    for prod in prod_list:
+        if prod.pid == pid:
+            found = True
+            return render(request,'MyShop/details.html',{'product':prod,})
+    if not found:
+        return render(request,'MyShop/details.html',{'product':None,})
