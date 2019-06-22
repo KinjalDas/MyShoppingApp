1dce38d12c6130be4ea458b096b8c936a4168f8e
Kaustav <kaustavsmailbox21@gmail.com>
2019-06-22 16:33:32 +0530

    latest changes

--- MyShoppingApp/urls.py
+++ MyShoppingApp/urls.py
@@ -19,6 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('MyAccounts.urls')),
-    path('cart/',include('MyCart.urls')),
     path('',include('MyShop.urls')),
 ]
