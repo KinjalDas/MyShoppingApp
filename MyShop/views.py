968cd83a1dc7550af22320905c78f5446ce5b652
Kinjal Das <kinjal.das86@gmail.com>
2019-06-27 21:12:51 +0530

    handling programming error due to default script at veiws.py of MyShop

--- MyShop/views.py
+++ MyShop/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.sessions.models import Session
+from django.db.utils import ProgrammingError
 
 from django.http import HttpResponse
 from django.template.loader import render_to_string
